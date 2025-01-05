a4276e8db807f66242f63220df7c0bc6f6bfabe3
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-01-06 00:28:38 +0900

    chore: add build option

--- native/build.rs
+++ native/build.rs
@@ -2,6 +2,8 @@ fn main () {
     csbindgen::Builder::default()
         .input_extern_file("src/lib.rs")
         .csharp_dll_name("native")
+        .csharp_namespace("UniXCF")
+        .csharp_class_accessibility("public")
         .generate_csharp_file("../Native.cs")
         .unwrap_or_else(|_| panic!("csbindgen: build failed"));
 }
